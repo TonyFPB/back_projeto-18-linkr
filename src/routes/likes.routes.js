c1305a9f9236b488c761251cac9be6a2fbdae83c
Sabta Mendes <sabtaamendes@gmail.com>
2023-01-10 15:35:22 -0400

    feat: id

--- src/routes/likes.routes.js
+++ src/routes/likes.routes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { getAllLikes, postLikes } from "../controllers/likes.controllers.js";
+import { getAllLikes, postLikes, removeLikes } from "../controllers/likes.controllers.js";
 import { validUsers } from "../middlewares/likes.middlewares.js";
 import { authTokenValidate } from "../middlewares/validateToken.middleware.js";
 
 const route = Router();
 
-route.post("/likes", authTokenValidate, validUsers, postLikes);
+route.post("/likes/:id", authTokenValidate, validUsers, postLikes);
 route.get("/likes/:id", authTokenValidate, validUsers, getAllLikes);
-route.delete("/likes",authTokenValidate, validUsers);
+route.delete("/likes:id",authTokenValidate, validUsers, removeLikes);
 export default route;
