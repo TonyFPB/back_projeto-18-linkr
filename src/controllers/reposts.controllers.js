d838c0f6fa00ebb5b58e47762994e26ac51d3c63
Lucas <lucass.ambar@gmail.com>
2023-01-11 19:28:16 -0300

    feat: get reposts

--- src/controllers/reposts.controllers.js
+++ src/controllers/reposts.controllers.js
@@ -1,31 +1,48 @@
-import {deleteFeed, insertRepostOnFeed} from "../repositories/reposts.repositories.js"
-import {insertPostHashtag} from "../repositories/posts.repositories.js"
+import {
+  deleteFeed,
+  insertRepostOnFeed,
+  selectReposts,
+} from "../repositories/reposts.repositories.js";
+import { insertPostHashtag } from "../repositories/posts.repositories.js";
 
 export async function repost(req, res) {
-    const { user_id, post_id, hashtags } = req.data;
-  
-    try {
-      await insertRepostOnFeed(post_id, user_id);
-  
-      if (hashtags.length === 0) return res.sendStatus(201);
-      for (let id of hashtags) await insertPostHashtag(post_id, id);
-  
-      res.sendStatus(201);
-    } catch (erro) {
-      console.log(erro);
-      res.sendStatus(500);
-    }
+  const { user_id, post_id, hashtags } = req.data;
+
+  try {
+    await insertRepostOnFeed(post_id, user_id);
+
+    if (hashtags.length === 0) return res.sendStatus(201);
+    for (let id of hashtags) await insertPostHashtag(post_id, id);
+
+    res.sendStatus(201);
+  } catch (erro) {
+    console.log(erro);
+    res.sendStatus(500);
   }
-  
-export async function unrepost (req, res) {
-    const {feed_id} = req.data
+}
 
-    try {
-        await deleteFeed(feed_id)
+export async function unrepost(req, res) {
+  const { feed_id } = req.data;
 
-        res.sendStatus(204)
-    } catch (erro) {
-        console.log(erro)
-        res.sendStatus(500)
-    }
+  try {
+    await deleteFeed(feed_id);
+
+    res.sendStatus(204);
+  } catch (erro) {
+    console.log(erro);
+    res.sendStatus(500);
+  }
+}
+
+export async function getReposts(req, res) {
+  const { post_id } = req.data;
+
+  try {
+    const  {rowCount} = await selectReposts(post_id)
+
+    res.send({rowCount})
+  } catch (erro) {
+    console.log(erro)
+    res.sendStatus(500)
+  }
 }
--- src/routes/reposts.routes.js
+++ src/routes/reposts.routes.js
@@ -1,13 +1,31 @@
-import express from "express"
+import express from "express";
 
 import { authTokenValidate } from "../middlewares/validateToken.middleware.js";
-import {validateRepost, validateUnrepost} from "../middlewares/reposts.middlewares.js"
-import {repost, unrepost} from "../controllers/reposts.controllers.js"
+import {
+  validateGetRepost,
+  validateRepost,
+  validateUnrepost,
+} from "../middlewares/reposts.middlewares.js";
+import {
+  getReposts,
+  repost,
+  unrepost,
+} from "../controllers/reposts.controllers.js";
 
-const router = express.Router()
+const router = express.Router();
 
 router.post("/repost/:post_id", authTokenValidate, validateRepost, repost);
-router.delete("/repost/:post_id", authTokenValidate, validateUnrepost, unrepost)
-router.get("/reposts")
+router.delete(
+  "/repost/:post_id",
+  authTokenValidate,
+  validateUnrepost,
+  unrepost
+);
+router.get(
+  "/repost/:post_id",
+  authTokenValidate,
+  validateGetRepost,
+  getReposts
+);
 
-export default router
+export default router;
