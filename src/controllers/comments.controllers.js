8749e2a9b52cb65f0958ce6760ce461760ed89aa
Antonio Breyer <antoniofabiopbreyer@gmail.com>
2023-01-12 14:14:36 -0300

    feat: get comments

--- src/controllers/comments.controllers.js
+++ src/controllers/comments.controllers.js
@@ -1,4 +1,4 @@
-import { insertComment } from "../repositories/comments.repositories.js"
+import { findWithComments,  insertComment } from "../repositories/comments.repositories.js"
 
 export async function postComments(req, res) {
     const { post_id } = req.params
@@ -7,6 +7,19 @@ export async function postComments(req, res) {
     try {
         await insertComment(post_id, user_id, comment)
         res.sendStatus(201)
+    } catch (err) {
+        console.log(err)
+        res.sendStatus(500)
+    }
+}
+
+export async function getComments(req, res) {
+    const user_id = res.locals
+    const { post_id } = req.params
+
+    try {
+        const comments = await findWithComments(user_id,post_id)
+        res.send(comments.rows)
     } catch (err) {
         console.log(err)
         res.sendStatus(500)
--- src/middlewares/comments.middlewares.js
+++ src/middlewares/comments.middlewares.js
@@ -2,7 +2,6 @@ import { selectPostById } from '../repositories/posts.repositories.js'
 
 export async function validatePostIdComments(req, res, next) {
     const { post_id } = req.params
-    
     try {
         if (isNaN(post_id)) {
             return res.sendStatus(404)
@@ -12,7 +11,6 @@ export async function validatePostIdComments(req, res, next) {
         if (postExists.rowCount === 0){
             return res.status(404).send({message:"Post not found."})
         }
-
     } catch (err) {
         console.log(err)
         res.send(500)
